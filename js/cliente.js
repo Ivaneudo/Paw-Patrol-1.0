17d8ecb43c563b053d814c85cd75414cf492298d
lin <laurasc1603@gmail.com>
2025-03-02 19:10:30 -0300

    js da mascara de CPF

---
+++ js/cliente.js
@@ -0,0 +1,12 @@
+let input = document.querySelector("#cpf")
+
+input.addEventListener('keypress', () => {
+     let inputlength = input.value.length
+
+     if (inputlength === 3 || inputlength === 7){
+	input.value += '.'
+     }
+     else if (inputlength === 11){
+	input.value += '-'
+     }
+})
