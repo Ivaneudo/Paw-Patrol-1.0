f67a4147c7ec5f8cf6d612c7d56c1ae09da396cb
lin <laurasc1603@gmail.com>
2025-03-02 21:05:24 -0300

    mascara do telefone

--- js/cliente.js
+++ js/cliente.js
@@ -1,12 +1,28 @@
-let input = document.querySelector("#cpf")
+let inputCpf = document.querySelector("#cpf")
 
-input.addEventListener('keypress', () => {
-     let inputlength = input.value.length
+inputCpf.addEventListener('keypress', () => {
+     let inputCpfLength = inputCpf.value.length
 
-     if (inputlength === 3 || inputlength === 7){
-	input.value += '.'
+     if (inputCpfLength === 3 || inputCpfLength === 7){
+	inputCpf.value += '.'
      }
-     else if (inputlength === 11){
-	input.value += '-'
+     else if (inputCpfLength === 11){
+	inputCpf.value += '-'
+     }
+})
+
+let inputTel = document.querySelector("#telefone")
+
+inputTel.addEventListener('keypress', () => {
+     let inputTelLength = inputTel.value.length
+
+     if (inputTelLength === 0){
+	inputTel.value = '(' + inputTel.value
+     }
+     if(inputTelLength === 3){
+	inputTel.value += ') '
+     }
+     else if(inputTelLength === 9){
+	inputTel.value += '-'
      }
 })
