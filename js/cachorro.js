cffbff162b59f9e75b423cf2908386dcf421c50d
Ivaneudo <159584361+Ivaneudo@users.noreply.github.com>
2025-03-08 14:11:01 -0300

    alterações

---
+++ js/cachorro.js
@@ -0,0 +1,11 @@
+const dog = document.querySelector("#dog");
+
+dog.addEventListener("click", () => {
+
+        console.log("clicou")
+
+        const cachorro = document.querySelector("#cachorro")
+    
+        cachorro.id = "";
+
+})
